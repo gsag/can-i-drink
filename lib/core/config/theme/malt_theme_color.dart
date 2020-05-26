0ce506de933c7d154c3abead1bbf3bd72e51dd8e
Guilherme Alves <gsag.dh@gmail.com>
2020-05-26 00:05:17 -0300

    REFS #1: Scaffolding new material app
    Scaffolding new material app and also adding a custom material color based on: http://cvi.09b.myftpupload.com/foundations/color/

---
+++ lib/core/config/theme/malt_theme_color.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class MaltThemeColor {
+  static const Color PRIMARY_RED_10 = Color(0xFFFE5D62);
+  static const Color PRIMARY_RED_30 = Color(0xFFDB393E);
+  static const Color PRIMARY_RED_50 = Color(0xFFB11F24);
+} //class
---
+++ lib/core/config/theme/malt_theme_data.dart
@@ -0,0 +1,43 @@
+import 'dart:math';
+import 'package:canidrink/core/config/theme/malt_theme_color.dart';
+import 'package:flutter/material.dart';
+
+class MaltThemeConfig {
+  static final ThemeData maltThemeData = ThemeData(
+    primarySwatch: _generateMaterialColor(MaltThemeColor.PRIMARY_RED_30),
+  );
+
+  //https://medium.com/@morgenroth/using-flutters-primary-swatch-with-a-custom-materialcolor-c5e0f18b95b0
+  static MaterialColor _generateMaterialColor(Color color) {
+    return MaterialColor(color.value, {
+      50: _tintColor(color, 0.9),
+      100: _tintColor(color, 0.8),
+      200: _tintColor(color, 0.6),
+      300: _tintColor(color, 0.4),
+      400: _tintColor(color, 0.2),
+      500: color,
+      600: _shadeColor(color, 0.1),
+      700: _shadeColor(color, 0.2),
+      800: _shadeColor(color, 0.3),
+      900: _shadeColor(color, 0.4),
+    });
+  }
+
+  static int _tintValue(int value, double factor) =>
+      max(0, min((value + ((255 - value) * factor)).round(), 255));
+
+  static Color _tintColor(Color color, double factor) => Color.fromRGBO(
+      _tintValue(color.red, factor),
+      _tintValue(color.green, factor),
+      _tintValue(color.blue, factor),
+      1);
+
+  static int _shadeValue(int value, double factor) =>
+      max(0, min(value - (value * factor).round(), 255));
+
+  static Color _shadeColor(Color color, double factor) => Color.fromRGBO(
+      _shadeValue(color.red, factor),
+      _shadeValue(color.green, factor),
+      _shadeValue(color.blue, factor),
+      1);
+} //class
