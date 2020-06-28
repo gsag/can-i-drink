4d577f23f623ce2770d10a6d1c7afbed887a3c34
Guilherme Alves <gsag.dh@gmail.com>
2020-06-28 16:23:08 -0300

    REFS #6: Adding simple splash animation (Flare)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:canidrink/core/config/theme/malt_theme_data.dart';
 import 'package:canidrink/view/barcode-scanner/barcode_scanner_page.dart';
+import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -15,15 +16,29 @@ class FlutterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final String appTitle = "Can I Drink?";
+    final String splashPath = "assets/splash/splash.flr";
+    final String splashAnimationName = "Splash";
+
     return MaterialApp(
-        title: appTitle,
-        theme: MaltThemeConfig.maltThemeData,
-        home: BlocProvider(
-          create: (context) => BarcodeScannerBloc(),
-          child: BarcodeScannerPage(
-            key: Key("BarcodeScannerPage"),
-            pageTitle: appTitle,
-          ),
-        ));
+      title: appTitle,
+      theme: MaltThemeConfig.maltThemeData,
+      home: SplashScreen.navigate(
+        name: splashPath,
+        startAnimation: splashAnimationName,
+        next: (context) => this._getHomeWidget(context, appTitle),
+        until: () => Future.delayed(Duration(seconds: 2)),
+      ),
+    );
+  } //func
+
+  Widget _getHomeWidget(BuildContext context, String appTitle) {
+    final provider = BlocProvider(
+      create: (context) => BarcodeScannerBloc(),
+      child: BarcodeScannerPage(
+        key: Key("BarcodeScannerPage"),
+        pageTitle: appTitle,
+      ),
+    );
+    return provider;
   } //func
 } //class
